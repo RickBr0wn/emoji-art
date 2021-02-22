8068793aadcc2a554761a442c5f7066ed04eb74c
Rick Brown <ricky.brown.00@gmail.com>
2021-02-22 10:29:19 +0000

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Rick Brown on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
